310eef7dc3bbc57dc00fa92ac8f6845c9f826a46
olip-03 <83119244+olip-03@users.noreply.github.com>
2025-09-03 02:01:58 +0000

    Deploying to gh-pages from  @ c0efe9e12c6b083b41df4d2e388b74095a929339 🚀

--- pkg/web.d.ts
+++ pkg/web.d.ts
@@ -1,13 +1,10 @@
-declare namespace wasm_bindgen {
-	/* tslint:disable */
-	/* eslint-disable */
-	export function main(): void;
-	
-}
+/* tslint:disable */
+/* eslint-disable */
+export function main(): void;
 
-declare type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
 
-declare interface InitOutput {
+export interface InitOutput {
   readonly memory: WebAssembly.Memory;
   readonly main: () => void;
   readonly __wbindgen_exn_store: (a: number) => void;
@@ -18,6 +15,17 @@ declare interface InitOutput {
   readonly __wbindgen_start: () => void;
 }
 
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
 /**
 * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
 * for everything else, calls `WebAssembly.instantiate` directly.
@@ -26,4 +34,4 @@ declare interface InitOutput {
 *
 * @returns {Promise<InitOutput>}
 */
-declare function wasm_bindgen (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
