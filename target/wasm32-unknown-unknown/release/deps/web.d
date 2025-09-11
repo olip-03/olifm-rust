e866292ef7fb902a8e2f88b81d31d4b62b70ae39
olip-03 <83119244+olip-03@users.noreply.github.com>
2025-09-11 13:38:07 +0000

    Deploying to gh-pages from  @ eb4594bab7b8e389954ab9951ac9cce31b4b9a04 🚀

--- target/wasm32-unknown-unknown/release/deps/web.d
+++ target/wasm32-unknown-unknown/release/deps/web.d
@@ -1,8 +1,8 @@
 /home/runner/work/olifm-rust/olifm-rust/web/target/wasm32-unknown-unknown/release/deps/web.d: src/lib.rs src/content.rs src/image.rs src/page.rs src/pages/mod.rs src/pages/macros.rs src/pages/page_about.rs src/pages/page_document.rs src/pages/page_home.rs src/pages/page_not_found.rs src/pages/page_pictures.rs src/pages/page_sounds.rs src/router.rs
 
-/home/runner/work/olifm-rust/olifm-rust/web/target/wasm32-unknown-unknown/release/deps/web.wasm: src/lib.rs src/content.rs src/image.rs src/page.rs src/pages/mod.rs src/pages/macros.rs src/pages/page_about.rs src/pages/page_document.rs src/pages/page_home.rs src/pages/page_not_found.rs src/pages/page_pictures.rs src/pages/page_sounds.rs src/router.rs
-
 /home/runner/work/olifm-rust/olifm-rust/web/target/wasm32-unknown-unknown/release/deps/libweb.rlib: src/lib.rs src/content.rs src/image.rs src/page.rs src/pages/mod.rs src/pages/macros.rs src/pages/page_about.rs src/pages/page_document.rs src/pages/page_home.rs src/pages/page_not_found.rs src/pages/page_pictures.rs src/pages/page_sounds.rs src/router.rs
+
+/home/runner/work/olifm-rust/olifm-rust/web/target/wasm32-unknown-unknown/release/deps/web.wasm: src/lib.rs src/content.rs src/image.rs src/page.rs src/pages/mod.rs src/pages/macros.rs src/pages/page_about.rs src/pages/page_document.rs src/pages/page_home.rs src/pages/page_not_found.rs src/pages/page_pictures.rs src/pages/page_sounds.rs src/router.rs
 
 src/lib.rs:
 src/content.rs:
